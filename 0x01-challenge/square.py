481a4b28460b8fbd1e1c7251ac6849271568965f
callewise <mudemifaustin@gmail.com>
2023-04-16 04:28:21 -0400

    added descriptive docstrings

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
+""" Module for square class"""
 
 class square():
+    """ Square class """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Initialize the square with its width and height. """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """ Calculate Area of the square. """
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ Calculate the perimeter of the square. """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Return a string representation of the square. """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
+    """ Create a square object """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
